e4c3b66c59a9f3b55c8faf8fc3c59d8b74ac6842
Muzamil.Shah <muzamilshahqurishy782787@gmail.com>
2023-09-27 17:06:02 +0530

    booking view

--- src/components/Bookings/BookingItem.tsx
+++ src/components/Bookings/BookingItem.tsx
@@ -6,14 +6,14 @@ import moment from 'moment'
 
 const BookingItem = ({id,name,price,time,status}: BookingProps) => {
   return (
-    <StyleView className='w-full h-32 mb-1 bg-orange-100 rounded-md p-3 flex flex-row justify-between items-center'>
+    <StyleView className='w-full h-28 mb-1 bg-orange-100 rounded-md p-3 flex flex-row justify-between items-center'>
       <StyleView className='justify-center items-start space-y-2 bg-transparent'>
-        <StyleText category='s1'>{name}</StyleText>
+        <StyleText category='s2'>{name}</StyleText>
         <StyleText category='label'>Price: {price.toString()}</StyleText>
       </StyleView>
-      <StyleView className='justify-center items-start space-y-2 bg-transparent'>
+      <StyleView className='justify-center items-end space-y-2 bg-transparent'>
         <StyleText category='label' status={status === 'Book' ? 'success' : 'danger'}>{status}</StyleText>
-        <StyleText category='label'>{moment(time).fromNow()}</StyleText>
+        <StyleText category='c1' >{moment(time).fromNow()}</StyleText>
       </StyleView>
     </StyleView>
   )
