96a4d55a9fe02327a41565e8e600d210af8003b3
Elham13 <elhamuddin.mahmoodi@gmail.com>
2023-10-04 14:27:51 +0530

    Added lotti animation in refer a friend

--- src/views/ReferAFriend.tsx
+++ src/views/ReferAFriend.tsx
@@ -1,8 +1,11 @@
-import {Share, StyleSheet, Text, View} from 'react-native';
+import {Share, StyleSheet, Dimensions, View} from 'react-native';
 import React from 'react';
 import PageHeader from '../components/PageHeader';
 import {StyleButton, StyleView} from '../utils/TailwindAndUIkiteCombination';
 import {generateCouponCode} from '../utils/helpers';
+import LottieView from 'lottie-react-native';
+
+const {width, height} = Dimensions.get('window');
 
 const ReferAFriend = () => {
   const shareCoupon = () => {
@@ -15,8 +18,22 @@ const ReferAFriend = () => {
   return (
     <View>
       <PageHeader title="Refer a friend" />
-      <StyleView className="p-8 h-full">
-        <StyleButton onPress={shareCoupon}>Share your coupon</StyleButton>
+      <StyleView className="p-8 h-full items-center">
+        <LottieView
+          style={{width, height: height / 4}}
+          source={require('../assets/json/sendingMessage.json')}
+          autoPlay
+          loop
+        />
+        <StyleButton onPress={shareCoupon} className="my-8 w-full">
+          Share your coupon
+        </StyleButton>
+        <LottieView
+          style={{width, height: height / 4}}
+          source={require('../assets/json/share.json')}
+          autoPlay
+          loop
+        />
       </StyleView>
     </View>
   );
