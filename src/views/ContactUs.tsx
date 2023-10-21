b46bb48063e6f2b33f6b8f4a39cd7218470251ee
Elham13 <elhamuddin.mahmoodi@gmail.com>
2023-10-22 02:53:30 +0530

    Safety videos done

--- src/views/ContactUs.tsx
+++ src/views/ContactUs.tsx
@@ -22,57 +22,55 @@ const ContactUs = () => {
   });
   return (
     <SafeAreaView>
-      <StyleView className="flex-1" level="2">
-        <PageHeader title="Contact us" />
-        <StyleView className="w-full h-[92%] bg-orange-100 justify-center items-center space-y-9 p-2">
-          <StyleView className="w-full h-40 bg-orange-500 rounded-3xl p-2 justify-center items-center">
-            <StyleText category="h5" status="control">
-              We are here for support
-            </StyleText>
-            <StyleText category="s1" status="control">
-              email : skyjumping@gmail.com
-            </StyleText>
-            <StyleText category="s1" status="control">
-              Phone : +91 9512222222 | 9511111111
-            </StyleText>
-          </StyleView>
-          <StyleView
-            className="relative w-11/12 h-96 space-y-4 overflow-hidden rounded-xl p-2 flex justify-end items-center px-6"
-            level="1">
-            <StyleInput
-              className="z-10"
-              onChangeText={nextValue =>
-                setContactFormData(pre => ({...pre, name: nextValue}))
-              }
-              value={contactFormData.name}
-              label={'Name'}
-              placeholder="Enter your Name"
+      <PageHeader title="Contact us" />
+      <StyleView className="bg-orange-100 justify-center items-center space-y-9 p-2">
+        <StyleView className="w-full h-40 bg-orange-500 rounded-3xl p-2 justify-center items-center">
+          <StyleText category="h5" status="control">
+            We are here for support
+          </StyleText>
+          <StyleText category="s1" status="control">
+            email : skyjumping@gmail.com
+          </StyleText>
+          <StyleText category="s1" status="control">
+            Phone : +91 9512222222 | 9511111111
+          </StyleText>
+        </StyleView>
+        <StyleView
+          className="relative w-11/12 h-96 space-y-4 overflow-hidden rounded-xl p-2 flex justify-end items-center px-6"
+          level="1">
+          <StyleInput
+            className="z-10"
+            onChangeText={nextValue =>
+              setContactFormData(pre => ({...pre, name: nextValue}))
+            }
+            value={contactFormData.name}
+            label={'Name'}
+            placeholder="Enter your Name"
+          />
+          <StyleInput
+            className="z-10"
+            onChangeText={nextValue =>
+              setContactFormData(pre => ({...pre, email: nextValue}))
+            }
+            value={contactFormData.email}
+            label={'email'}
+            placeholder="Enter your Name"
+          />
+          <StyleInput
+            className="z-10"
+            onChangeText={nextValue =>
+              setContactFormData(pre => ({...pre, message: nextValue}))
+            }
+            value={contactFormData.message}
+            label={'Message'}
+            placeholder="Write your message here..."
+          />
+          <StyleButton className="z-10">Submit</StyleButton>
+          <StyleView className="w-40 h-40 rounded-full justify-end items-center overflow-hidden  absolute -top-24 translate-x-1 bg-orange-100 z-0">
+            <StyleImage
+              className="w-20 h-20 "
+              source={require('./../assets/monkey.png')}
             />
-            <StyleInput
-              className="z-10"
-              onChangeText={nextValue =>
-                setContactFormData(pre => ({...pre, email: nextValue}))
-              }
-              value={contactFormData.email}
-              label={'email'}
-              placeholder="Enter your Name"
-            />
-            <StyleInput
-              className="z-10"
-              onChangeText={nextValue =>
-                setContactFormData(pre => ({...pre, message: nextValue}))
-              }
-              value={contactFormData.message}
-              label={'Message'}
-              placeholder="Write your message here..."
-            />
-            <StyleButton className="z-10">Submit</StyleButton>
-            <StyleView className="w-40 h-40 rounded-full justify-end items-center overflow-hidden  absolute -top-24 translate-x-1 bg-orange-100 z-0">
-              <StyleImage
-                className="w-20 h-20 "
-                source={require('./../assets/monkey.png')}
-              />
-            </StyleView>
           </StyleView>
         </StyleView>
       </StyleView>
--- src/views/SafetyVideo.tsx
+++ src/views/SafetyVideo.tsx
@@ -1,5 +1,6 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import YoutubePlayer from 'react-native-youtube-iframe';
+import {StyleSheet} from 'react-native';
 import {StyleView} from '../utils/TailwindAndUIkiteCombination';
 import PageHeader from '../components/PageHeader';
 
@@ -7,7 +8,7 @@ const SafetyVideo = () => {
   return (
     <StyleView className="flex-1">
       <PageHeader title="Safety Videos" />
-      <Text>SafetyVideo</Text>
+      <YoutubePlayer height={300} videoId={'jVM_C97uRMM'} />
     </StyleView>
   );
 };
